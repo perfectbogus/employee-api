be60c2ce4da7c739c4094e710e2fd191774d1307
perfectbogus <war_army_10@hotmail.com>
2026-09-10 16:47:11 -0600

    Commit base project

---
+++ src/test/java/dev/perfectbogus/employeeapi/EmployeeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.perfectbogus.employeeapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
