9832277ffed14635fdf26545f626a6b9656f692a
perfectbogus <war_army_10@hotmail.com>
2026-09-11 10:46:34 -0600

    Base configuration project

---
+++ src/main/java/dev/perfectbogus/employeeapi/exception/EmployeeNotFoundException.java
@@ -0,0 +1,7 @@
+package dev.perfectbogus.employeeapi.exception;
+
+public class EmployeeNotFoundException extends RuntimeException {
+  public EmployeeNotFoundException(String message) {
+    super(message);
+  }
+}
